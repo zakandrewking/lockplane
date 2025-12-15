2de79a032cd5bb650aed2427a943d2261589c6c2
Zachary King <zak@delfina.com>
2025-12-14 16:29:18 -0800

    force cmd

--- cmd/init.go
+++ cmd/init.go
@@ -17,10 +17,12 @@ var initCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+	initCmd.Flags().Bool("force", false, "Overwrite existing lockplane.toml file")
 }
 
 func runInit(cmd *cobra.Command, args []string) {
-	if err := wizard.Run(); err != nil {
+	force, _ := cmd.Flags().GetBool("force")
+	if err := wizard.Run(force); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
